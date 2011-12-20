a77d563c516cd5e2df3563f97a27396d5891f9b8
errs81 <errs81@gmail.com>
2011-12-20 14:35:44 +0000

    Proyecto Registro

---
+++ errs-registro/src/Sesion.java
@@ -0,0 +1,11 @@
+
+
+public class Sesion {
+
+	public Usuario getAtributo(Usuario usuario) {
+		
+		
+		return usuario;
+	}
+
+}
---
+++ errs-registro/src/UsuarioNoExisteException.java
@@ -0,0 +1,11 @@
+
+public class UsuarioNoExisteException extends Exception {
+	public UsuarioNoExisteException()
+	{
+		super();
+	}
+	public UsuarioNoExisteException(String s)
+	{
+		super(s);
+	}
+}
